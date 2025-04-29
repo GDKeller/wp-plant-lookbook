1761a3e59ee53b612575a7a26efae3e7e3f4659d
Grant Keller <grant@kellerdigital.com>
2025-04-29 07:51:31 +0000

    Installed and ran PHPCS on plugin

--- plugins/plants-lookbook-plugin/render/block-biome.php
+++ plugins/plants-lookbook-plugin/render/block-biome.php
@@ -7,37 +7,37 @@
  * @package Plants Lookbook
  */
 
-$name               = $attributes["biomeName"] ?? "";
-$content_position   = $attributes["biomeContentPosition"] ?? "bottomleft";
-$image              = $attributes["biomeImage"];
-$image_url          = ! empty( $image["url"] ) ? $image["url"] : "";
-$image_alt          = ! empty( $image["alt"] ) ? $image["alt"] : $name;
+$name             = $attributes['biomeName'] ?? '';
+$content_position = $attributes['biomeContentPosition'] ?? 'bottomleft';
+$image            = $attributes['biomeImage'];
+$image_url        = ! empty( $image['url'] ) ? $image['url'] : '';
+$image_alt        = ! empty( $image['alt'] ) ? $image['alt'] : $name;
 
-$classes            = ["wp-block-plants-lookbook-biome", "alignfull"];
-$instance_id        = wp_unique_id( "wp-block-plants-lookbook-biome-" );
+$classes     = array( 'wp-block-plants-lookbook-biome', 'alignfull' );
+$instance_id = wp_unique_id( 'wp-block-plants-lookbook-biome-' );
 
 $classes[] = $content_position;
 
-if ( ! empty( $attributes["className"] ) ) {
-    $classes[] = $attributes["className"];
+if ( ! empty( $attributes['className'] ) ) {
+	$classes[] = $attributes['className'];
 }
 
-$classes_str = implode( " ", $classes );
+$classes_str = implode( ' ', $classes );
 ?>
 
 <section class="<?php echo esc_attr( $classes_str ); ?>" role="region" aria-labelledby="<?php echo esc_attr( $instance_id ); ?>">
-    <div class="wp-block-plants-lookbook-biome__info">
-        <div class="wp-block-plants-lookbook-biome__info-inner">
-            <h2 id="<?php echo esc_attr( $instance_id ); ?>" class="wp-block-plants-lookbook-biome__name"><?php echo esc_html( $name ); ?></h2>
-            <div class="wp-block-plants-lookbook-biome__description">
-                <?php echo wp_kses_post( $content ); ?>
-            </div>
-        </div>
-    </div>
+	<div class="wp-block-plants-lookbook-biome__info">
+		<div class="wp-block-plants-lookbook-biome__info-inner">
+			<h2 id="<?php echo esc_attr( $instance_id ); ?>" class="wp-block-plants-lookbook-biome__name"><?php echo esc_html( $name ); ?></h2>
+			<div class="wp-block-plants-lookbook-biome__description">
+				<?php echo wp_kses_post( $content ); ?>
+			</div>
+		</div>
+	</div>
 
-    <?php if ( $image_url ) : ?>
-        <div class="wp-block-plants-lookbook-biome__image-container">
-            <img src="<?php echo esc_url( $image_url ); ?>" alt="<?php echo esc_attr( $image_alt ); ?>" loading="lazy" />
-        </div>
-    <?php endif; ?>
+	<?php if ( $image_url ) : ?>
+		<div class="wp-block-plants-lookbook-biome__image-container">
+			<img src="<?php echo esc_url( $image_url ); ?>" alt="<?php echo esc_attr( $image_alt ); ?>" loading="lazy" />
+		</div>
+	<?php endif; ?>
 </section>
--- plugins/plants-lookbook-plugin/render/block-species.php
+++ plugins/plants-lookbook-plugin/render/block-species.php
@@ -7,36 +7,36 @@
  * @package Plants Lookbook
  */
 
-$align		= $attributes["align"] ?? "";
-$name		= $attributes["speciesCommonName"] ?? "";
-$binomial	= $attributes["speciesFormalName"] ?? "";
-$desc		= $attributes["speciesDescription"] ?? "";
+$align    = $attributes['align'] ?? '';
+$name     = $attributes['speciesCommonName'] ?? '';
+$binomial = $attributes['speciesFormalName'] ?? '';
+$desc     = $attributes['speciesDescription'] ?? '';
 
-$image		= $attributes["speciesImage"];
-$image_url	= ! empty( $image["url"] ) ? $image["url"] : "";
-$image_alt	= ! empty( $image["alt"] ) ? $image["alt"] : $name;
+$image     = $attributes['speciesImage'];
+$image_url = ! empty( $image['url'] ) ? $image['url'] : '';
+$image_alt = ! empty( $image['alt'] ) ? $image['alt'] : $name;
 
-$classes = ["wp-block-plants-lookbook-species"];
-$instance_id = wp_unique_id( "wp-block-plants-lookbook-species-" );
+$classes     = array( 'wp-block-plants-lookbook-species' );
+$instance_id = wp_unique_id( 'wp-block-plants-lookbook-species-' );
 
-if ( ! empty( $attributes["flipped"] ) ) {
-	$classes[] = "is-flipped";
+if ( ! empty( $attributes['flipped'] ) ) {
+	$classes[] = 'is-flipped';
 }
 if ( ! empty( $align ) ) {
-	$classes[] = "align" . $align;
+	$classes[] = 'align' . $align;
 }
-if ( ! empty( $attributes["className"] ) ) {
-	$classes[] = $attributes["className"];
+if ( ! empty( $attributes['className'] ) ) {
+	$classes[] = $attributes['className'];
 }
 
-$classes_str = implode( " ", $classes );
-?>	
+$classes_str = implode( ' ', $classes );
+?>    
 
 <section class="<?php echo esc_attr( $classes_str ); ?>" role="region" aria-labelledby="<?php echo esc_attr( $instance_id ); ?>">
 	<div class="wp-block-plants-lookbook-species__info">
 		<div class="wp-block-plants-lookbook-species__info-inner">
 			<h2 id="<?php echo esc_attr( $instance_id ); ?>" class="wp-block-plants-lookbook-species__name"><?php echo esc_html( $name ); ?></h2>
-			<?php if ( ! empty( $binomial ) ) :	 ?>
+			<?php if ( ! empty( $binomial ) ) : ?>
 				<p class="wp-block-plants-lookbook-species__binomial"><?php echo esc_html( $binomial ); ?></p>
 			<?php endif; ?>
 			<?php if ( ! empty( $desc ) ) : ?>
